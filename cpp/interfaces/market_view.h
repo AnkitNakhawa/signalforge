4baa203f7d35c679619e94915536a9c8ba81be41
AnkitNakhawa <ankitnakhawa@gmail.com>
2026-01-21 22:06:22 -0500

    added market view (read-only) interface

---
+++ cpp/interfaces/market_view.h
@@ -0,0 +1,18 @@
+// cpp/interfaces/market_view.h
+#pragma once
+#include "cpp/orderbook/order_book.h" // for Price, Quantity, Side
+
+namespace signalforge {
+
+class MarketView {
+public:
+    virtual ~MarketView() = default;
+
+    virtual bool has_top() const = 0;      // best bid/ask available
+    virtual Price best_bid() const = 0;
+    virtual Price best_ask() const = 0;
+    virtual bool has_last() const = 0;     // last trade available
+    virtual Price last_price() const = 0;
+};
+
+}
